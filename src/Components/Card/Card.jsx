a881b7c2837eaf037af83db3ecac80a16a2aa98a
janiceh94 <haesung94@gmail.com>
2022-10-05 10:34:28 -0700

    images work

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './Card.css';
 import Star from '../../Images/Star 1.png'
-import Cottage from '../../Images/cottage-gbb028a51e_1920.jpg'
 
 const Card = (props) => {
     return (
         <div className="card">
-            {/* <img className="card-image" src={`${props.cardImg}`}alt='cottage'/> */}
-            <img className="card-image" src={Cottage}alt='cottage'/> 
+            <img className="card-image" src={require(`../../Images/${props.cardImg}`)}alt='cottage'/>
             <div className='card-stats'>
                 <img className='card-star' src ={Star} alt='star'/>
                 <span>{props.rating}</span>
