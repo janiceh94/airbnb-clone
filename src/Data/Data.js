c3025678bcb45816f40b42d5679a320aff191155
janiceh94 <haesung94@gmail.com>
2022-10-05 09:51:35 -0700

    created data

---
+++ src/Data/Data.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        id: 1,
+        title: "House in the country side",
+        price: 135,
+        coverImg: "cottage-gbb028a51e_1920.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "New Zealand",
+    },
+    {
+        id: 2,
+        title: "House by the lake",
+        price: 160,
+        coverImg: "lake-g2e438a177_1280.jpg",
+        stats: {
+            rating: 4.5,
+            reviewCount: 2
+        },
+        location: "Yosemite",
+    },
+    {
+        id: 3,
+        title: "House by the beach",
+        price: 180,
+        coverImg: "large-home-g3db68ad14_1280.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 10
+        },
+        location: "Malibu",
+    },
+]
